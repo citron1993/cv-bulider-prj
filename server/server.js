2247c8d9167b126233b4940e28b915b7d559514b
citron1993 <citron25693@gmail.com>
2026-01-24 14:42:38 +0200

    added basic endpoints for saving and retreving the cv data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
 const express =require("express")
 
 const app=express();
+const fs=require("fs");
 const port =3000;
+
+let cvData={}
+// לקחת את האוביקט מהבדי מתווח שיודע
+app.use(express.json());
+
+app.post("/api/cv/save",(req,res)=>{
+const {fullName,phone,email,summery,educationObject,skills,expiriences}=req.body;
+cvData={fullName,phone,email,summery,educationObject,skills,expiriences}
+fs.writeFileSync(cvData)
+const stringifiedCvData=json.stringify(cvData)
+console.log("./cvData.json",stringifiedCvData);
+
+res.send("cv data saved successfuly")
+
+})
+
+app.get("/api/cv",(req,res)=>{
+const cv=fs.readFileSync("./cvData.json")  
+res.send(cv)  
+})
 
 app.listen(port,()=>{
     console.log(`started listening on port ${port}` );
